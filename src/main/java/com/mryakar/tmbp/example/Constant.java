dd8dc9a6ea4e999ede423653497e0d99350a67e8
Ahmet Yakar <ahmetyakar@pm.me>
2023-12-23 17:15:09 +0300

    Add Constant.java for all scenarios

---
+++ src/main/java/com/mryakar/tmbp/example/Constant.java
@@ -0,0 +1,14 @@
+package com.mryakar.tmbp.example;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class Constant {
+    public static final String SEQUENCE_GENERATOR_NAME = "seq_generator";
+    public static final String HOUSES_TABLE_NAME = "Houses";
+    public static final String HOUSES_SEQUENCE_NAME = "Houses_seq";
+    public static final String WIZARDS_TABLE_NAME = "Wizards";
+    public static final String WIZARDS_SEQUENCE_NAME = "Wizards_seq";
+
+}
